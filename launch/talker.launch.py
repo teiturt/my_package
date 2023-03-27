14718a1c250e6a45de2f0b4aa96ea5c351ae8c12
Teitur <sigurdurtt20@ru.is>
2023-03-27 22:15:03 +0000

    changed demo to cpp

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package="demo_nodes_cpg",
+            package="demo_nodes_cpp",
             executable='talker'
         )
     ])
